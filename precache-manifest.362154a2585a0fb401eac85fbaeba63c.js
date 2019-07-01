b0288135f42ff8e4e32113106731767efc9ff41d
RobertPurcea <purcearobert@gmail.com>
2019-07-01 11:15:12 +0100

    Updates

---
+++ precache-manifest.362154a2585a0fb401eac85fbaeba63c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92cc5d7e89e8b599c612f66d010587c9",
+    "url": "/smartpensiontest/index.html"
+  },
+  {
+    "revision": "c04214b8ee251addb8a3",
+    "url": "/smartpensiontest/static/css/main.724410d9.chunk.css"
+  },
+  {
+    "revision": "7e850f2e74fb10be7d68",
+    "url": "/smartpensiontest/static/js/2.8ad9deff.chunk.js"
+  },
+  {
+    "revision": "c04214b8ee251addb8a3",
+    "url": "/smartpensiontest/static/js/main.47d0220f.chunk.js"
+  },
+  {
+    "revision": "2057c745e7d0d02d531c",
+    "url": "/smartpensiontest/static/js/runtime~main.2028403d.js"
+  }
+]);
